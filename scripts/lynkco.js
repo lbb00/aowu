3c5f9d3c279e57ef8bf50352edbc2f39e332323c
onelong <love@onelong.org>
2020-01-09 01:56:53 +0800

    feat: 优化领克分享滑动屏幕的参数

--- scripts/lynkco.js
+++ scripts/lynkco.js
@@ -4,12 +4,12 @@ function task() {
   maid.launch()
   waitForActivity('com.geely.lynkco.main.activity.LynkCoTabMainActivity')
   sleep(6000)
-  swipe(300, 1700, 300, 200, 200)
-  sleep(1200)
+  swipe(300, 1700, 300, 200, 300)
+  sleep(2000)
   let shareCount = 0
   while (shareCount < 3) {
-    swipe(300, 1700, 300, 200, 200)
-    sleep(1200)
+    swipe(300, 1700, 300, 200, 300)
+    sleep(2000)
     const list = descMatches(/.*[天时秒]前$/).find()
     for (let i = 0; i < list.length; i++) {
       const obj = list[i]
@@ -21,7 +21,7 @@ function task() {
           .child(1)
           .child(3)
           .click()
-        sleep(2000)
+        sleep(3000)
         clickWidget(desc('微信好友').findOnce())
         shareCount++
         waitForActivity('com.tencent.mm.ui.transmit.SelectConversationUI')
