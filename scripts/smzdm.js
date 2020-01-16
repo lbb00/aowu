8a6fdbf007379426e119cb7552c544a2d2b85200
onelong <love@onelong.org>
2020-01-17 01:39:07 +0800

    fix: 修复什么值得买更新弹窗导致签到失败

--- scripts/smzdm.js
+++ scripts/smzdm.js
@@ -4,7 +4,11 @@ function task() {
   maid.launch()
   waitForActivity('com.smzdm.client.android.activity.HomeActivity')
   sleep(2000)
-  const upEntry = text('签到福利').findOnce()
+  const cancelUpdateBtn = id('iv_content_cancel').findOnce()
+  if (cancelUpdateBtn) {
+    cancelUpdateBtn.click()
+  }
+  sleep(2000)
   if (upEntry) {
     clickWidget(upEntry)
     waitForActivity('com.smzdm.client.android.user_center.signin.SignInActivity')
