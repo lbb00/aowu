aeef7466657ee632c441247edc7491a57ae44e6c
onelong <love@onelong.org>
2020-01-17 01:22:48 +0800

    fix: 修复联通营业厅弹窗导致签到失败

--- scripts/联通营业厅.js
+++ scripts/联通营业厅.js
@@ -3,6 +3,14 @@ const maid = getMaid('com.sinovatech.unicom.ui')
 function task() {
   maid.launch()
   waitForActivity('com.sinovatech.unicom.basic.ui.activity.MainActivity')
+  sleep(2000)
+  const cancelBtn = text(
+    'oFr5vJX3UQPSC9zxHDaogEvWoQcnnqMLcSRw18J6ZKGBYq3rYYwU+bFUeVgLHba6x4zbn8bzKjtu8Nnnc5p8AAwDgFJ01yE0rBgAAAABJRU5ErkJggg=='
+  ).findOnce()
+  if (cancelBtn) {
+    cancelBtn.click()
+    sleep(1000)
+  }
   id('main_tab_user_layout').click()
   sleep(2000)
   const upEntry = text('我的金币').findOnce()
