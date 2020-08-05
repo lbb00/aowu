0bd2728c0ee222d7b5fd84e46488bcab3af36019
onelong <love@onelong.org>
2020-08-06 00:19:20 +0800

    fix: 海底捞签到

--- scripts/海底捞.js
+++ scripts/海底捞.js
@@ -1,12 +1,28 @@
 const maid = getMaid('com.haidilao')
 
+function closeIV () {
+  if (id('iv_closeevaluate').findOnce()) {
+    id('iv_closeevaluate').findOnce().click()
+  }
+  sleep(1000)
+  if (id('iv_close').findOnce()) {
+    id('iv_close').findOnce().click()
+  }
+  sleep(1000)
+}
+
 function task () {
   maid.launch()
   waitForActivity('com.haidilao.hailehui.biz.impl.activity.MainActivity')
   sleep(2000)
-  click(953 + 5, 272 + 5)
-  waitForActivity('com.haidilao.hailehui.biz.impl.activity.SecondActivity')
-  sleep(6000)
+
+  closeIV()
+  const btn = desc('签到').findOnce()
+  if (btn) {
+    clickWidget(btn)
+    waitForActivity('com.haidilao.hailehui.biz.impl.activity.SecondActivity')
+    sleep(5000)
+  }
   maid.close()
 }
 
